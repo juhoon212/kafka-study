8ff0a57ecd4fb777e98c04323ba728b3e5b72f3c
juhoon212 <ws5501@naver.com>
2025-09-20 22:12:19 +0900

    study: kafka-producer send message to topic

--- producers/src/main/java/com/example/producers/SimpleProducer.java
+++ producers/src/main/java/com/example/producers/SimpleProducer.java
@@ -1,20 +1,32 @@
 package com.example.producers;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class SimpleProducer {
     public static void main(String[] args) {
+        String topic = "simple-topic";
         // KafkaProducer configuration setting
         Properties props = new Properties();
         // boostrap.servers, key.serializer.class, value.serializer.class
         //props.setProperty("bootstrap.servers", "192.168.64.22:9092");
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.64.22:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.219.212:9092");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // KafkaProducer object creation
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        // ProducerRecord object creation
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "id-001", "hello-world");
+
+        // KafkaProducer send data to Kafka topic
+        producer.send(record);
+        producer.flush();
+        producer.close();
 
     }
 }
