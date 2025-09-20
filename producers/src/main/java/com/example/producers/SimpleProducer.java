1df87d8f56ebfa927dbb343a05744ca0d718ebab
juhoon212 <ws5501@naver.com>
2025-09-20 21:30:08 +0900

    study: kafka-producer config

--- producers/src/main/java/com/example/producers/SimpleProducer.java
+++ producers/src/main/java/com/example/producers/SimpleProducer.java
@@ -1,5 +1,20 @@
 package com.example.producers;
 
-public class SimpleProducer {
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        // KafkaProducer configuration setting
+        Properties props = new Properties();
+        // boostrap.servers, key.serializer.class, value.serializer.class
+        //props.setProperty("bootstrap.servers", "192.168.64.22:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.64.22:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+    }
 }
