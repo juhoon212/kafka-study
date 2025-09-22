46f469ba6fad55b83a759b1f1655698350d3355f
juhoon212 <ws5501@naver.com>
2025-09-22 21:25:12 +0900

    study: kafka-producer send message sync

---
+++ producers/src/main/java/com/example/producers/SimpleProducerSync.java
@@ -0,0 +1,44 @@
+package com.example.producers;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class SimpleProducerSync {
+    public static final Logger logger = LoggerFactory.getLogger(SimpleProducerSync.class.getName());
+    public static void main(String[] args) {
+        String topic = "simple-topic";
+        // KafkaProducer configuration setting
+        Properties props = new Properties();
+        // boostrap.servers, key.serializer.class, value.serializer.class
+        //props.setProperty("bootstrap.servers", "192.168.64.22:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.219.212:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer object creation
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        // ProducerRecord object creation
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "id-001", "hello-world");
+
+        // KafkaProducer send data to Kafka topic
+        Future<RecordMetadata> future = producer.send(record);
+        try {
+            // sync code - .get() blocks the .send() method until a response is received
+            final RecordMetadata recordMetadata = future.get();
+            logger.info("\n ##### record metadata received #### \n partition: {}\noffset: {}\ntimestamp: {}", recordMetadata.partition(), recordMetadata.offset(), recordMetadata.timestamp());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            producer.close();
+        }
+    }
+}
