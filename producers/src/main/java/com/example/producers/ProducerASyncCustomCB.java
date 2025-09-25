62e25625d3c1c12d48495e92d3efad77cf30c601
juhoon212 <ws5501@naver.com>
2025-09-25 21:59:58 +0900

    study: kafka-producer send to multi partitions and callback customization

--- producers/src/main/java/com/example/producers/ProducerASyncCustomCB.java
+++ producers/src/main/java/com/example/producers/ProducerASyncCustomCB.java
@@ -1,16 +1,18 @@
 package com.example.producers;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerASyncWithKey {
-    public static final Logger logger = LoggerFactory.getLogger(ProducerASyncWithKey.class.getName());
+public class ProducerASyncCustomCB {
+    public static final Logger logger = LoggerFactory.getLogger(ProducerASyncCustomCB.class.getName());
     public static void main(String[] args) {
         String topic = "multipart-topic";
         // KafkaProducer configuration setting
@@ -18,24 +20,18 @@ public class ProducerASyncWithKey {
         // boostrap.servers, key.serializer.class, value.serializer.class
         //props.setProperty("bootstrap.servers", "192.168.64.22:9092");
         props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.219.212:9092");
-        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // KafkaProducer object creation
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
         // ProducerRecord object creation
-        ProducerRecord<String, String> record = null;
-        for (int i=0; i<20; ++i) record = new ProducerRecord<>(topic, String.valueOf(i), "hello-world" + i);
-
-        // KafkaProducer send data to Kafka topic
-        producer.send(record, (metadata, exception) -> {
-            if (exception == null) {
-                logger.info("\n ##### record metadata received #### \n partition: {}\noffset: {}\ntimestamp: {}",
-                        metadata.partition(), metadata.offset(), metadata.timestamp());
-            } else {
-                logger.error("exception error from broker - {}", exception.getMessage());
-            }
-        });
+        for (int i=0; i<20; ++i) {
+            Callback callback = new CustomCallback(i);
+            ProducerRecord<Integer, String> record = new ProducerRecord<>(topic, i, "hello-world" + i);
+            // KafkaProducer send data to Kafka topic
+            producer.send(record, callback);
+        }
 
         try {
             Thread.sleep(3000);
