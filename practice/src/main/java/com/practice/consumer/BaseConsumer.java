e1bf80dd406c5b56a3ea9683397d9d186d068aa5
juhoon212 <ws5501@naver.com>
2026-02-25 20:38:21 +0900

    feat: prepare base consumer

--- practice/src/main/java/com/practice/consumer/BaseConsumer.java
+++ practice/src/main/java/com/practice/consumer/BaseConsumer.java
@@ -1,23 +1,25 @@
 package com.practice.consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.time.Duration;
 import java.util.List;
+import java.util.Properties;
 
 public class BaseConsumer<K extends Serializable, V extends Serializable> {
 
     public static Logger logger = LoggerFactory.getLogger(BaseConsumer.class);
 
-    private KafkaConsumer<K, V> kafkaConsumer;
-    private List<String> topics;
+    private final KafkaConsumer<K, V> kafkaConsumer;
+    private final List<String> topics;
 
-    public BaseConsumer(KafkaConsumer<K, V> kafkaConsumer, List<String> topics) {
-        this.kafkaConsumer = kafkaConsumer;
+    public BaseConsumer(Properties consumerProps, List<String> topics) {
+        this.kafkaConsumer = new KafkaConsumer<>(consumerProps);
         this.topics = topics;
     }
 
@@ -48,5 +50,72 @@ public class BaseConsumer<K extends Serializable, V extends Serializable> {
 
     private void processRecords(ConsumerRecords<K, V> records) {
         records.forEach(this::processRecord);
+    }
+
+    public void pollConsumes(long durationMillis, String commitMode) {
+        try {
+            while (true) {
+                if (commitMode.equals("sync")) {
+                    pollCommitSync(durationMillis);
+                } else {
+                    pollCommitAsync(durationMillis);
+                }
+            }
+        }catch(WakeupException e) {
+            logger.error("wakeup exception has been called");
+        }catch(Exception e) {
+            logger.error(e.getMessage());
+        }finally {
+            logger.info("##### commit sync before closing");
+            kafkaConsumer.commitSync();
+            logger.info("finally consumer is closing");
+            closeConsumer();
+        }
+    }
+
+    private void pollCommitAsync(long durationMillis) throws WakeupException, Exception {
+        ConsumerRecords<K, V> consumerRecords = this.kafkaConsumer.poll(Duration.ofMillis(durationMillis));
+        processRecords(consumerRecords);
+        this.kafkaConsumer.commitAsync( (offsets, exception) -> {
+            if(exception != null) {
+                logger.error("offsets {} is not completed, error:{}", offsets, exception.getMessage());
+            }
+
+        });
+
+    }
+    private void pollCommitSync(long durationMillis) throws WakeupException, Exception {
+        ConsumerRecords<K, V> consumerRecords = this.kafkaConsumer.poll(Duration.ofMillis(durationMillis));
+        processRecords(consumerRecords);
+        try {
+            if(consumerRecords.count() > 0 ) {
+                this.kafkaConsumer.commitSync();
+                logger.info("commit sync has been called");
+            }
+        } catch(CommitFailedException e) {
+            logger.error(e.getMessage());
+        }
+    }
+    public void closeConsumer() {
+        this.kafkaConsumer.close();
+    }
+
+    public static void main(String[] args) {
+        String topicName = "file-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.64.25:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "file-group");
+        props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
+        BaseConsumer<String, String> baseConsumer = new BaseConsumer<>(props, List.of(topicName));
+        baseConsumer.initConsumer();
+        String commitMode = "async";
+
+        baseConsumer.pollConsumes(100, commitMode);
+        baseConsumer.closeConsumer();
+
     }
 }
